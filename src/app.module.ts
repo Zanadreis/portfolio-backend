8b543e24862a6d80d8de9266ccf4fbd93e0ad9b6
Zanadreis <matheusolvic@hotmail.com>
2024-09-25 19:12:47 -0300

    feat: root url with text

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Controller, Get, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UrlModule } from './modules/url/url.module';
 import { HealthModule } from './modules/health/health.module';
 import { LocationModule } from './modules/location/location.module';
 import { PersonModule } from './modules/person/person.module';
+import { ApiExcludeController } from '@nestjs/swagger';
 
 @Module({
   imports: [
@@ -22,4 +23,13 @@ import { PersonModule } from './modules/person/person.module';
   controllers: [],
   providers: [],
 })
+
+@ApiExcludeController()
+@Controller()
+class AppController {
+  @Get()
+  root() {
+    return 'API made by Zanadreis. See /docs for more information.';
+  }
+}
 export class AppModule {}
